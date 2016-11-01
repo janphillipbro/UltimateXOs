ba4411c3ad3eb76bd5f37c6f15616d3e29f12d1d
Julian Rengstorf <julian.rengstorf@nordakademie.de>
2016-11-01 14:04:59 +0000

    init

---
+++ rengstorf_julian_2926988_assignment2/src/Example020.java
@@ -0,0 +1,43 @@
+
+
+// JavaFX example that showcases how to build an entirely custom control
+// that fits into the JavaFX model.
+// imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+// class definition
+public class Example020 extends Application {
+	// overridden init method
+	@Override
+	public void init() {
+		// initialise the stack pane and add a custom control to it
+		sp_mainlayout = new StackPane();
+		cc_custom = new CustomControl();
+		sp_mainlayout.getChildren().add(cc_custom);
+	}
+
+	// overridden start method
+	public void start(Stage primaryStage) {
+		// set a size, title and a scene on the main window. show it when
+		// ready
+		primaryStage.setTitle("Custom Control Example");
+		primaryStage.setScene(new Scene(sp_mainlayout, 300, 300));
+		primaryStage.show();
+	}
+
+	// overridden stop method
+	public void stop() {
+	}
+
+	// entry point into our program to launch our JavaFX application
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	// private fields for this class
+	private StackPane sp_mainlayout;
+	private CustomControl cc_custom;
+}
